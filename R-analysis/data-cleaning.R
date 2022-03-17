a4ec3bd75c168d31b20740d8340def25a3e59000
Peter M Grund <87161151+petermgrund@users.noreply.github.com>
2022-03-17 18:56:16 -0500

    Added title and data import to cleaning file

--- R-analysis/data-cleaning.R
+++ R-analysis/data-cleaning.R
@@ -1,1 +1,14 @@
-# DATA CLEANING DOC
+##################################################################
+## PSY 4960 FINAL PROJECT
+## DATA CLEANING DOCUMENT
+##
+## Created March 2022
+## Authors: Peter M. Grund
+## Additional project contributors: 
+##################################################################
+
+
+# Import data and dependencies --------------------------------------------
+
+library(tidyverse)
+df <- read_csv("In-data/emerging_adult_data.csv")

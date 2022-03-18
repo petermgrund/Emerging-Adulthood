17cc7ba4e98f6e6dc69a6fae0dc4c5acaa82b7b7
Peter M Grund <87161151+petermgrund@users.noreply.github.com>
2022-03-17 19:16:50 -0500

    Added code for row IDs and variable selection

--- R-analysis/data-cleaning.R
+++ R-analysis/data-cleaning.R
@@ -1,14 +1,25 @@
-##################################################################
+# # # # # # # # # # # # # # # # # # # # 
 ## PSY 4960 FINAL PROJECT
 ## DATA CLEANING DOCUMENT
-##
+## 
 ## Created March 2022
 ## Authors: Peter M. Grund
 ## Additional project contributors: 
-##################################################################
-
+# # # # # # # # # # # # # # # # # # # # 
 
 # Import data and dependencies --------------------------------------------
 
+#load tidyverse
 library(tidyverse)
+
+# This open-source data file can be found at https://osf.io/te54b/
 df <- read_csv("In-data/emerging_adult_data.csv")
+
+# Create IDs for rows -----------------------------------------------------
+
+df$ID <- 1:nrow(df)
+
+# Select variables of interest --------------------------------------------
+
+df_select <- df %>%
+  select(ID, mindful_1:mindful_biascheck)
